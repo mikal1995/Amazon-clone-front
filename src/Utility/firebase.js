a715e6a40d23486386e056c8f4b3a8b2ab91417b
mikal1995 <mikalefrs@gmail.com>
2024-08-13 10:41:03 -0400

    fire updated

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -4,14 +4,27 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB5me4dkSIxE1YybiMXYpR53PBJQXpFkfo",
+//   authDomain: "clone-ba803.firebaseapp.com",
+//   projectId: "clone-ba803",
+//   storageBucket: "clone-ba803.appspot.com",
+//   messagingSenderId: "377752001158",
+//   appId: "1:377752001158:web:1d91098103a027097d8f07"
+// };
+
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyB5me4dkSIxE1YybiMXYpR53PBJQXpFkfo",
   authDomain: "clone-ba803.firebaseapp.com",
   projectId: "clone-ba803",
   storageBucket: "clone-ba803.appspot.com",
   messagingSenderId: "377752001158",
-  appId: "1:377752001158:web:1d91098103a027097d8f07"
+  appId: "1:377752001158:web:0a967574bd7e4d387d8f07"
 };
+
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 export const auth = getAuth(app);
